c73a3e60ff25c6ffbdb4ebc22b9c3fbbb90f4f07
Ramaraja <r.ramaraja@yahoo.com>
2023-10-31 18:55:59 -0400

    Fix Chat

--- src/components/chats.jsx
+++ src/components/chats.jsx
@@ -1,32 +1,6 @@
 import React, { useEffect, useRef, useState } from "react";
 import socketIO from "socket.io-client";
-import { randomChat } from "../utils/randomChat";
-
-const Avator = (props)  =>{
-  const avatarLinsl = [
-    {titile: "StakeHolder1", link: "https://api.dicebear.com/7.x/miniavs/svg?seed=lisa", color: "bg-yellow-500	"},
-    {titile: "StakeHolder2", link: "https://api.dicebear.com/7.x/miniavs/svg?seed=john", color: "bg-green-500"},
-    {titile: "Developer1", link: "https://api.dicebear.com/7.x/miniavs/svg?seed=jennie", color: "bg-cyan-500"},
-    {titile: "Developer2", link: "https://api.dicebear.com/7.x/miniavs/svg?seed=mike", color: "bg-rose-500"},
-    {titile: "TeamLeader", link: "https://api.dicebear.com/7.x/miniavs/svg?seed=michelle", color: ""},
-    {titile: "You", link: "https://api.dicebear.com/7.x/miniavs/svg?seed=nothing", color: ""}, 
-  ]
-
-  const getSource = (title) => {
-    return avatarLinsl.filter((avatar) => avatar.titile === title)[0].link
-  }
-
-  return (
-    <div>
-      <div className="w-12 aspect-square">
-        <img
-          src={getSource(props.title)}
-          alt="avatar" 
-        />
-      </div>
-    </div>
-  )
-}
+import Avatar from "../components/avatar";
 
 const Chats = (props) => {
   const [messages, setMessages] = useState([]);
@@ -36,69 +10,39 @@ const Chats = (props) => {
   const [waiting, setWaiting] = useState(false);
 
   useEffect(() => {
-    if (process.env.REACT_APP_MODE==="ui"){
-      //setMessages(chats.chats);
-      
-    }
-    else {
-      socketRef.current = socketIO.connect("http://localhost:3001");
-      socketRef.current.on("GPTResponse", (data) => {
-        const gptResponse = data.filter((message) => message.type === "AI");
-        console.log(gptResponse);
-        console.log(data);
-        setMessages((prevMessages) => [...prevMessages, ...gptResponse]);
-      });
-    }
+    socketRef.current = socketIO.connect("http://localhost:3001");
+    socketRef.current.on("GPTResponse", (data) => {
+      const gptResponse = data.filter((message) => message.type === "AI");
+      console.log(gptResponse);
+      console.log(data);
+      setMessages((prevMessages) => [...prevMessages, ...gptResponse]);
+    });
+
+    return () => {
+      socketRef.current.disconnect();
+    };
   }, []);
 
   useEffect(() => {
     lastMessageRef.current?.scrollIntoView({ behavior: "smooth" });
   }, [messages]);
 
-  function timeout(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-  }
-
   const handleSendMessage = async (e) => {
     e.preventDefault();
 
     if (message.trim()) {
-      
-      
-      
-      if (process.env.REACT_APP_MODE === "ui"){
-        const messageObject = {
-          name: "You", 
-          text: message, 
-          id: `${Math.random()}`, 
-        }
-
-        setMessage("");
-        setMessages((prevMessages) => [...prevMessages, messageObject]);
-        setWaiting(true);
-       
-        const resps = randomChat();
-        console.log(resps);
-        for (const resp of resps) {
-          await timeout(1000);
-          setMessages((prevMessages) => [...prevMessages, resp]);
-        }
-
-        setWaiting(false);
-      }
-      else{
-        const messageObject = {
-          type: "human", 
-          name: "You", 
-          text: message, 
-          id: `${socketRef.current.id}${Math.random()}`, 
-          socketID: socketRef.current.id
-        }
-        setMessages((prevMessages) => [...prevMessages, messageObject])
-        socketRef.current.emit("userMessage", messageObject);
-
-        setMessage("");
-      }
+      const messageObject = {
+        type: "human",
+        name: "You",
+        text: message,
+        id: `${socketRef.current.id}${Math.random()}`,
+        socketID: socketRef.current.id,
+      };
+      setMessages((prevMessages) => [...prevMessages, messageObject]);
+      setWaiting(true);
+      socketRef.current.emit("userMessage", messageObject);
+      setWaiting(false);
+      setMessage("");
     }
   };
 
@@ -114,7 +58,7 @@ const Chats = (props) => {
                 index % 2 === 0 ? "bg-chat" : "bg-chat-accent"
               } flex flex-row w-full px-11 py-5 justify-center items-start gap-3`}
             >
-              <Avator title={message.name} />
+              <Avatar name={message.name} />
               <div className="flex flex-col w-2/3">
                 <div className="font-semibold">{message.name}</div>
                 <p className="text-sm ">{message.text}</p>
@@ -122,9 +66,12 @@ const Chats = (props) => {
             </li>
           );
         })}
-        {waiting && <li>
-          <p>Waiting for agents...</p>
-        </li>}
+        {/* Implement a loading indicator here instead of text message */}
+        {waiting && (
+          <li>
+            <p>Waiting for agents...</p>
+          </li>
+        )}
       </ul>
 
       <div className="sticky bottom-0 w-full bg-chat-accent flex flex-row just-around gap-1 shadow-xl">
@@ -134,7 +81,6 @@ const Chats = (props) => {
             value={message}
             onChange={(e) => {
               setMessage(e.target.value);
-
             }}
             className="
               h-8
