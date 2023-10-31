2365cb50b3d4f3658f3edb4ce7e9e7c7d5ae3de0
Ramaraja <r.ramaraja@yahoo.com>
2023-10-31 18:55:35 -0400

    Move Avatar to a new component

---
+++ src/components/avatar.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+const Avatar = (props) => {
+  const avatarList = [
+    {
+      name: "Sarah",
+      link: "https://api.dicebear.com/7.x/miniavs/svg?seed=lisa",
+      color: "bg-yellow-500	",
+    },
+    {
+      name: "stakeholder1",
+      link: "https://api.dicebear.com/7.x/miniavs/svg?seed=john",
+      color: "bg-green-500",
+    },
+    {
+      name: "developer",
+      link: "https://api.dicebear.com/7.x/miniavs/svg?seed=jennie",
+      color: "bg-cyan-500",
+    },
+    {
+      name: "teamlead",
+      link: "https://api.dicebear.com/7.x/miniavs/svg?seed=michelle",
+      color: "bg-rose-500",
+    },
+    { name: "You", link: "https://api.dicebear.com/7.x/miniavs/svg?seed=nothing", color: "" },
+  ];
+
+  const getSource = (name) => {
+    return avatarList.filter((avatar) => avatar.name === name)[0].link;
+  };
+
+  return (
+    <div>
+      <div className="w-12 aspect-square">
+        <img src={getSource(props.name)} alt="avatar" />
+      </div>
+    </div>
+  );
+};
+
+export default Avatar;
