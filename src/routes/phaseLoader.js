943df8a241dd5159d9a0440118ec35890379d968
Ramaraja <r.ramaraja@yahoo.com>
2023-11-06 19:17:45 -0500

    Update phaseLoader.js

--- src/routes/phaseLoader.js
+++ src/routes/phaseLoader.js
@@ -4,26 +4,19 @@ export async function projectsLoader({ params }) {
       id: "design@project-1",
       name: "Project 1",
       phase: "design",
-      chats: [
-        { id: "design@project-1@intial-sketch", name: "Initial Sketch" },
-        { id: "design@project-1@design-1", name: "Design 1" },
-      ],
+      chats: [{ id: "design@project-1@design-1", name: "Design 1" }],
     },
     {
       id: "design@project-2",
       name: "Project 2",
       phase: "design",
-      chats: [
-        { id: "design@project-2@intial-sketch", name: "Initial Sketch" },
-        { id: "design@project-2@design-1", name: "Design 1" },
-      ],
+      chats: [{ id: "design@project-2@design-1", name: "Design 1" }],
     },
     {
       id: "3@design",
       name: "Project 3",
       phase: "design",
       chats: [
-        { id: "design@project-3@intial-sketch", name: "Initial Sketch" },
         { id: "design@project-3@design-1", name: "Design 1" },
         { id: "design@project-3@design-2", name: "Design 2" },
       ],
